d5c97fa8ca3bd62b902b3f3581ab4b418125ee6c
banumariwan <mariwanburhan5@gmail.com>
2025-11-16 04:56:10 +0300

    Add Category model and nested CategorySerializer in PostSerializer

--- posts/models.py
+++ posts/models.py
@@ -3,10 +3,21 @@ from django.db import models
 # Create your models here.
 
 
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    categories = models.ForeignKey(Category,on_delete=models.CASCADE, related_name='posts',null=True,blank=True)
 
     def __str__(self):
         return self.title
--- posts/serializers.py
+++ posts/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post,Category
+
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id','name']
+
 
 
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('id', 'title', 'content', 'created_at')
+        fields = ['id', 'title', 'content', 'created_at', 'categories']
